04bf9fd352989613c876bf4a9cfb091c2d2d45e6
Fergus Farrin <ff323@ic.ac.uk>
2025-06-06 11:02:51 +0100

    finished MCS

--- Monte_Carlo/montecarlo.jl
+++ Monte_Carlo/montecarlo.jl
@@ -1,23 +1,22 @@
 using IntervalArithmetic
 
-function interval_monte_carlo_option_price(S0, K, sigma, r, T, n)
+function MonteCarlo(S0, K, sigma, r, T, n)
 
-    total = Interval(0.0)
+    total = interval(0.0)
     
     for _ in 1:n
-        Z = randn()
+        Z = randn() #standard normal random sample
         
         exponent = (r - sigma^2 / 2) * T + sigma * sqrt(T) * Z
-        S_T = S0 * exp(exponent)
+        S_T = S0 * exp(exponent) #geometric Brownian motion
         
         payoff = max(S_T - K, 0.0)
-        discounted_payoff = exp(-r * T) * payoff
+        discounted_payoff = exp(-r * T) * payoff #
         
         total += discounted_payoff
     end
-    
     option_price_interval = total / n
     return option_price_interval
 end
 
-println(interval_monte_carlo_option_price(5950, 5900, interval(0.0441, 0.0446), interval(0.1836, 0.2045), 30/365, 1000))
+println(MonteCarlo(5950, 5000, interval(0.0441, 0.0446), interval(0.1836, 0.2045), 30/365, 10000))
